5d48011886202468f0daf90f116b8852c8a64a7d
zqw <zqw-mac@zhaoqinweideMacBook-Pro.local>
2020-04-07 14:29:15 +0800

    ribbon

--- cloud-consumer-order80/src/main/java/com/qw/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/qw/springcloud/controller/OrderController.java
@@ -2,7 +2,11 @@ package com.qw.springcloud.controller;
 
 import com.qw.springcloud.entities.CommonResult;
 import com.qw.springcloud.entities.Payment;
+import com.qw.springcloud.lb.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,13 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @Slf4j
 public class OrderController {
-    public static final String PAYMENT_URL = "http://cloud-payment-service";
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
     @Resource
     private RestTemplate restTemplate;
+    @Resource
+    private LoadBalancer loadBalancer;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/consumer/payment/create")
     public CommonResult create(Payment payment) {
@@ -26,5 +36,31 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult(444, "请求未响应,id:" + id);
+        }
+    }
+
+    @GetMapping("/consumer/payment/getLb")
+    public CommonResult getPaymentLb() {
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if(serviceInstances == null || serviceInstances.size() <= 0){
+            return null;
+        }
+        ServiceInstance serviceInstance = loadBalancer.instance(serviceInstances);
+        URI uri = serviceInstance.getUri();
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(uri + "/payment/get/" + 1, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult(444, "请求未响应,id:" + 1);
+        }
     }
 }
